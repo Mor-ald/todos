9865cd65ea55aa9d17a0f8e1922c717894c9f6ed
Mor-ald <mor.ald@yandex.ru>
2025-01-21 19:13:52 +0300

    feature(components): add InitialTodosData

---
+++ src/components/todos/InitialTodosData.ts
@@ -0,0 +1,24 @@
+import { Todo } from "../../ts/Todo.ts";
+
+/**
+ * Initial data for Todos component
+ */
+const InitialTodosData: Todo[] = [
+	{
+		id: 1,
+		text: "Тестовое задание",
+		completed: false,
+	},
+	{
+		id: 2,
+		text: "Прекрасный код",
+		completed: true,
+	},
+	{
+		id: 1,
+		text: "Покрытие тестами",
+		completed: false,
+	},
+];
+
+export default InitialTodosData;
