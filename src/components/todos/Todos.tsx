3ed32831632ba1bd5daa0c11d14bcdfd805bf9ae
Mor-ald <mor.ald@yandex.ru>
2025-01-21 19:14:20 +0300

    feature(components): add Todos component & logic

---
+++ src/components/todos/Todos.tsx
@@ -0,0 +1,74 @@
+import styles from "./Todos.module.css";
+import { useCallback, useState } from "react";
+import { Todo } from "../../ts/Todo.ts";
+import InitialTodosData from "./InitialTodosData.ts";
+import { CurrentTodosDisplay } from "../../ts/CurrentTodosDisplay.ts";
+
+/**
+ * Todos component
+ */
+export default function Todos() {
+	const [todos, setTodos] = useState<Todo[]>(InitialTodosData);
+	const [visibleDropDown, setVisibleDropDown] = useState<boolean>(true);
+	const [currentTodosDisplay, setCurrentTodosDisplay] = useState<CurrentTodosDisplay>("All");
+	const [filterFeature, setFilterFeature] = useState<boolean>(false);
+
+	/**
+	 * Add new todo in todos
+	 */
+	const addTodo = useCallback(
+		(text: string) => {
+			return setTodos([...todos, { id: todos.length, text: text, completed: false }]);
+		},
+		[todos],
+	);
+
+	/**
+	 * Complete todo
+	 */
+	const completeTodo = useCallback(
+		(id: number) => {
+			const newTodos = [...todos];
+			newTodos.find((todo) => todo.id === id)!.completed = true;
+
+			return setTodos(newTodos);
+		},
+		[todos],
+	);
+
+	/**
+	 * Remove completed todos
+	 */
+	const removeCompletedTodos = useCallback(() => {
+		return setTodos(todos.filter((todo) => !todo.completed));
+	}, [todos]);
+
+	/**
+	 * Filter todos (all/active/completed)
+	 */
+	const filterTodos = useCallback(() => {
+		switch (currentTodosDisplay) {
+			case "Active":
+				return todos.filter((todo) => !todo.completed);
+			case "Completed":
+				return todos.filter((todo) => todo.completed);
+			default:
+				return todos;
+		}
+	}, [currentTodosDisplay, todos]);
+
+	/**
+	 * Switch filter type by button
+	 */
+	const onSwitchFilterType = useCallback((display: CurrentTodosDisplay) => {
+		setCurrentTodosDisplay(display);
+		setFilterFeature(display === "Active" || display === "Completed");
+	}, []);
+
+	return (
+		<div className={styles["todos-container"]}>
+			<div className={styles["todos-header"]}>todos</div>
+			<div className={styles["todos-content"]}></div>
+		</div>
+	);
+}
